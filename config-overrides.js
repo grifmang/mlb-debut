d8b671dd15840c81688b2b0378ed7833c3ba83e1
Tim Griffith <grifmang@gmail.com>
2025-01-04 22:53:51 -0500

    more fixes

---
+++ config-overrides.js
@@ -0,0 +1,13 @@
+module.exports = function override(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify"),
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify"),
+      "util": require.resolve("util/"),
+      "path": require.resolve("path-browserify"),
+    };
+    return config;
+  };
+  
